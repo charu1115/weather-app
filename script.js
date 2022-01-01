f9f2d2a8d9d675beb1027475f4d4fd9d53aa0b16
Prabhkirat <prabhkirat05@gmail.com>
2022-01-01 21:52:11 +0530

    Styles Added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
 const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
 async function getWeatherByLocation(city) {
   const resp = await fetch(url(city), {
     origin: "cros"
@@ -11,6 +12,7 @@ async function getWeatherByLocation(city) {
   const respData = await resp.json();
   addWeatherToPage(respData);
 }
+
 function addWeatherToPage(data) {
   const temp = Ktoc(data.main.temp);
   const weather = document.createElement('div')
@@ -23,6 +25,7 @@ function addWeatherToPage(data) {
   main.innerHTML = "";
   main.appendChild(weather);
 };
+
 function Ktoc(K) {
   return Math.floor(K - 273.15);
 }
@@ -30,6 +33,25 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   const city = search.value;
   if (city) {
-    getWeatherByLocation(city)
+    getWeatherByLocation(city);
   }
-});  
+});
+
+
+
+
+
+function backgroundChange() {
+
+  var imgs = document.getElementById("document");
+
+  if (data.weather[0].main == Rain) {
+    imgs.style.backgroundImage = "url(https://images.unsplash.com/photo-1519692933481-e162a57d6721?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80)";
+  } else if (data.weather[0].main == Clouds) {
+    imgs.style.backgroundImage = "url(https://images.unsplash.com/photo-1519692933481-e162a57d6721?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80)";
+  } else if (data.weather[0].main == Clear) {
+    imgs.style.backgroundImage = "url(https://images.unsplash.com/photo-1519692933481-e162a57d6721?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80)";
+  } else {
+    imgs.style.backgroundImage = "url(background.gif)";
+  }
+}
